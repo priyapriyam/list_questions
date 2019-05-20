3db64a5de83cd685b06f949cae3a32772307028e
priyapriyam <priyam18@navgurukul.org>
2019-05-20 21:49:19 +0530

    In this question i have to found out avg of sum even and odd numbers,so by using loop,if else,and divide oprator.

---
+++ avg_even_odd_list.py
@@ -0,0 +1,26 @@
+elements=[23,14,56,12,19,9,15,31,42,43]
+i=0
+j=0
+new=[]
+new1=[]
+sum=0
+sum1=0
+while i<len(elements):
+    if (elements[i])%2==0:
+        new.append(elements[i])
+        sum=sum+(elements[i])
+    else:
+        new1.append(elements[i])
+        sum1=sum1+(elements[i])    
+    i=i+1
+print len(new)
+print len(new1)
+print sum
+print sum1
+avg_even=124/4
+print avg_even
+avg_odd=140/6
+print avg_odd
+
+
+
