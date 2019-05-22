359191f27409785b0446f893e92abb5b5ed09828
priyapriyam <priyam18@navgurukul.org>
2019-05-22 11:51:03 +0530

    in this list i have to find out index number of every elements

---
+++ find_index.py
@@ -0,0 +1,6 @@
+#22. Write a Python program to find the index of an item in a specified list. Go to the editor
+list=[12,34,556,6,2,4,56,7,8,9,87,9]
+i=0
+while i< len(list):
+    print list[i],"-",i
+    i=i+1
